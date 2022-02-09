1c394333aa23a87ac4f82b66ee720aa1fcb7e4fe
Sergjio97 <89703124+Sergjio97@users.noreply.github.com>
2022-02-09 18:34:24 +0100

    Add route link

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -14,6 +14,7 @@
                     <li><span>Series: </span><p>{{$comic['series']}}</p></li>
                     <li><span>Sale date: </span><p>{{$comic['sale_date']}}</p></li>
                     <li><span>Type: </span><p>{{$comic['type']}}</p></li>
+                    <li><a href="{{route('comics.show', $comic['id'])}}">Visualizza</a></li>
                 </ul>
             </li>
         @endforeach
